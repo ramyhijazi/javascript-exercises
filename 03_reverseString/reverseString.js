bd70f3caa6679503937146208d15d8246b6374d0
ramyhijazi <ramydhtr@gmail.com>
2024-07-17 14:37:27 -0500

    Completed removeFromArray.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // Split the string into an array of individual letters.
+    let splitString = string.split("")
 
+    // Reverse the array.
+    let reversedArray = splitString.reverse()
+
+    // Join the characters in the array.
+    let reversedString = reversedArray.join("")
+
+    // Return the final result.
+    return reversedString;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args)
+{
+// Create a blank new array.
+let newArray = [];
+// Iterate through the array passed in.
+array.forEach((item) => {
+    if (!args.includes(item)) {
+        newArray.push(item);
+    }
+})
 
+return newArray;
+};
+// Check if the current item is mentioned in args.
+
+// If it is not, then add it (push) to the new array.
+
+// return the new array.
 // Do not edit below this line
 module.exports = removeFromArray;
